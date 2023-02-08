1728c3495ad1362cbd7ae7ae443b8bb5e4474ff3
Aditya Pathak <sumitpathak861@gmail.com>
2023-02-08 20:41:46 +0530

    skill class made.

--- resume/main/models.py
+++ resume/main/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 from ckeditor.fields import RichTextField
 
+class Skill(models.Model):
+    class Meta:
+        verbose_name_plural = 'Skills'
+        verbose_name = 'Skill'
+
 
 
 
