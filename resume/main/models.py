365984fd61cca1ab10825fe04aa5f67da55cc990
Aditya Pathak <sumitpathak861@gmail.com>
2023-02-08 20:49:02 +0530

    Skill and User Profile created in models.py.

--- resume/main/models.py
+++ resume/main/models.py
@@ -8,6 +8,16 @@ class Skill(models.Model):
         verbose_name_plural = 'Skills'
         verbose_name = 'Skill'
 
+    name = models.CharField(max_length=20,blank=True,null=True)
+    score = models.IntegerField(default=80,blank=True,null=True)
+    image = models.FileField(blank=True,null=True,upload_to="Skills")
+    ia_key_skill = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name
 
+class UserProfile(models.Model):
 
+    class Meta:
+        verbose_name_plural = 'User Profiles'
+        verbose_name = 'User Profile'
